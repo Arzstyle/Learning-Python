abcf92e5ec0cc280e04dc0ee62395b49a6208fce
Muhamad Akbar Rizky S <akbarrizky0909@gmail.com>
2024-02-13 11:46:36 +0700

    Mencoba menyelesaikan tugas logic

---
+++ Tugas1/KonversiSuhu.py
@@ -0,0 +1,14 @@
+Reamur = float(input (" Masukan Suhu : " ))
+
+Celcius = 0.0
+Fahrenheit = 0.0
+Kelvin = 0.0
+
+rumusCelcius = 5/4 * Reamur
+rumusFahrenheit = 9/4 * Reamur + 32
+rumusKelvin = 5/4 * Reamur + 273.15
+
+print("Suhu Reamur %2.f -> Celcius : %.2f" %(Reamur, rumusCelcius))
+print("Suhu Reamur %2.f -> Fahrenheit : %.2f" %(Reamur, rumusFahrenheit))
+print("Suhu Reamur %2.f -> Kelvin : %.2f" %(Reamur, rumusKelvin))
+
---
+++ Tugas1/LuasKelSegitiga.py
@@ -0,0 +1,12 @@
+Alas = float(input ("Masukan Jumlah Alas : "))
+Tinggi = float(input ("Masukan Jumlah Tinggi : "))
+
+SisiA = 20
+SisiB = 12
+SisiC = 10
+
+rumusLuas = 1/2 * Alas * Tinggi
+rumusKeliling = SisiA + SisiB + SisiC
+
+print("Luas : %0.f" %rumusLuas)
+print("Keliling : %0.f" %rumusKeliling)
