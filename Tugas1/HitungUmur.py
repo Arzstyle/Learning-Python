873a057e6f5f6523dbdf8d803e677f380276c69f
Muhamad Akbar Rizky S <akbarrizky0909@gmail.com>
2024-02-15 19:44:26 +0700

    Mencoba mengejarkan logic hitung umur

---
+++ Tugas1/HitungUmur.py
@@ -0,0 +1,6 @@
+tahunLahir = int(input("Masukan Tahun Lahir : "))
+tahunSekarang = int(input("Masukan Tahun Sekarang : "))
+
+rumus = tahunSekarang - tahunLahir
+
+print("Umur saya sekarang adalah %s" % rumus)
