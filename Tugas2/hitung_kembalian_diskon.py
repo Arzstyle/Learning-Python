1f14383850921975b4d6923f228d169744e92857
Muhamad Akbar Rizky S <akbarrizky0909@gmail.com>
2024-02-25 17:39:59 +0700

    Merevisi code agar clean

--- Tugas2/hitung_kembalian_diskon.py
+++ Tugas2/hitung_kembalian_diskon.py
@@ -1,12 +1,13 @@
 uangBelanja = float (input (" Masukan uang belanja : "))
 totalBarang = float (input(" Masukan total barang : "))
 
+kembalian = uangBelanja - totalBarang 
+
 if totalBarang > 70.000:
     diskon = totalBarang * (10/100)
-    print(f"Sisa kembalian anda dengan diskon adalah : {diskon:0.3f} rupiah")
-else: diskon = 0
-
-totalBelanja = uangBelanja - totalBarang + diskon
-print(f"Sisa kembalian adalah : {totalBelanja:0.3f} rupiah")
+    kembalian += diskon
+    print(f"Sisa kembalian anda dengan diskon adalah : {kembalian:0.3f} rupiah")
+else: 
+    print(f"Sisa kembalian anda tanpa diskon adalah : {kembalian:0.3f} rupiah")
     
     
