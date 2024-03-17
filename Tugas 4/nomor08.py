5e0007cbf7b86dd277be406d90070015345c9bd9
Muhamad Akbar Rizky S <akbarrizky0909@gmail.com>
2024-03-17 23:38:18 +0700

    mengerjakan gunting kertas batu

--- Tugas 4/nomor08.py
+++ Tugas 4/nomor08.py
@@ -1,1 +1,23 @@
 #Program yang mensimulasikan permainan batu-gunting-kertas antara dua pemain dan menentukan pemenangnya.
+
+p1 = input("Player 1 memilih : ")
+com = input("Computer memilih : ")
+
+pick_1 = "batu"
+pick_2 = "gunting" 
+pick_3 = "kertas"
+
+if p1 == pick_1 and com == pick_2:
+  print(f"Player 1 menang")
+elif p1 == pick_2 and com == pick_1:
+  print(f"Player 2 menang")
+elif p1 == pick_1 and com == pick_3:
+  print(f"Player 2 menang")
+elif p1 == pick_3 and com == pick_1:
+  print(f"Player 1 menang")
+elif p1 == pick_2 and com == pick_3:
+  print(f"Player 1 menang")
+elif p1 == pick_3 and com == pick_2:
+  print(f"Player 2 menang")
+else:
+  print(f"Hasil seimbang")
--- Tugas 4/nomor11.py
+++ Tugas 4/nomor11.py
@@ -6,24 +6,36 @@
 
 # Ke 4 Kriteria diatas akan diterima hanya dan hanya jika tidak memiliki anggota tubuh yang cacat.
 
+anggota_tubuh_cacat = input("Apakah bagian tubuh anda memiliki cacat? " )
+usia = int(input("Masukan Umur Anda : "))
+jenis_kelamin = input("Masukan Jenis Kelamin Anda : ")
 berat_badan = int(input("Masukan Berat Badan Anda : "))
 tinggi_badan = int(input("Masukan Tinggi Badan Anda : "))
 usia = int(input("Masukan Umur Anda : "))
-jenis_kelamin = input("Masukan Jenis Kelamin Anda : ")
 rata_rata = int(input("Masukan Rata Rata Akademis : "))
 skill = input("Apakah Anda Memiliki Skill? : ")
 
-kecacatan = True or False
 
-if kecacatan == False: 
-  print("Anda Tidak Lolos karena tidak memenuhi kritea anggota Tubuh")
-elif (jenis_kelamin == "Laki laki" or jenis_kelamin == "Perempuan") and rata_rata >= 90 and usia <= 30:
-  print(f"Selamat Anda Lulus!, Anda bejernis kelamin {jenis_kelamin}, dengan rata rata akademis {rata_rata}, dan berusia {usia} Tahun.")
-elif jenis_kelamin == "Perempuan" and berat_badan >= 45 and berat_badan <= 50 and tinggi_badan >= 165 and usia <= 20: 
-  print(f"Selamat Anda Lulus!, Anda bejernis kelamin {jenis_kelamin}, dengan berat badan {berat_badan} kg, dan Tinggi {tinggi_badan}, dan berusia {usia} Tahun.")
-elif jenis_kelamin == "Laki laki" and berat_badan <= 70 and tinggi_badan >= 170 and usia <= 25:
-  print(f"Selamat Anda Lulus!, Anda bejernis kelamin {jenis_kelamin}, dengan berat badan {berat_badan} kg, dan Tinggi {tinggi_badan}, dan berusia {usia} Tahun.")
-elif (jenis_kelamin == "Laki Laki" or jenis_kelamin == "Perempuan") and skill == "Menembak" or skill == "Memanah" or skill == "Berkuda":
-  print(f"Selamat Anda Lulus!, Anda bejernis kelamin {jenis_kelamin}, karena kamu memiliki skill {skill}.")
+if anggota_tubuh_cacat == "memiliki": 
+  print("Anda tidak lolos karena memiliki anggota tubuh yang cacat")
 else:
-  print("Nilai yang anda masukan tidak sesuai dengan kritea yang di inginkan.")
+  if jenis_kelamin == "Perempuan" and berat_badan >= 45:
+    if berat_badan <= 50 and tinggi_badan >= 165 and usia <= 20: 
+      print(f"Anda Lulus Kriteria 1!, bejernis kelamin {jenis_kelamin}, dengan berat badan {berat_badan} kg, dan Tinggi {tinggi_badan}, dan berusia {usia} Tahun.")
+    else:
+      print(f"Anda tidak memenuhi kriteria ")
+  elif jenis_kelamin == "Laki laki" and berat_badan <= 70:
+    if tinggi_badan >= 170 and usia <= 25:
+      print(f"Anda Lulus Kriteria 2!, bejernis kelamin {jenis_kelamin}, dengan berat badan {berat_badan} kg, dan Tinggi {tinggi_badan}, dan berusia {usia} Tahun.")
+    else:
+      print(f"Anda tidak memenuhi kriteria ")
+  if jenis_kelamin == "Laki laki" or jenis_kelamin == "Perempuan":      
+    if rata_rata >= 90 and usia <= 30:
+      print(f"Anda lulus kriteria 3!, bejernis kelamin {jenis_kelamin}, dengan rata rata akademis : {rata_rata}, dan berusia {usia} Tahun.")
+    else:
+      print("Anda tidak memenuhi kriteria 3")
+  if jenis_kelamin == "Laki Laki" or jenis_kelamin == "Perempuan": 
+    if skill == "Menembak" or skill == "Memanah" or skill == "Berkuda":
+      print(f"Anda lulus kriteria 4, Anda bejernis kelamin {jenis_kelamin}, karena kamu memiliki skill {skill}.")
+    else:
+      print("Anda tidak memenuhi kriteria ")
