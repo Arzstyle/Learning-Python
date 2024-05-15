85bca7f29683d395f4b67f80791381485c62350f
M Akbar Rizky Saputra <akbarrizky0909@gmail.com>
2024-05-15 10:45:19 +0700

    mempersingkat code

--- Tugas 11/num02.py
+++ Tugas 11/num02.py
@@ -2,10 +2,18 @@
 # Contoh parameter: my_list = [1, 2, 3, 4, 5]
 # Contoh output: 15
 
-def menambahkan(my_list):
-    total = 0
-    for angka in my_list:
-        total += angka
-    return total
+# def menambahkan(my_list):
+#     total = 0
+#     for angka in my_list:
+#         total += angka
+#     return total
+
+# print(menambahkan([1, 2, 3, 4, 5]))
+
+
+
+def menambahkan(mylist):
+    return sum(mylist)
 
 print(menambahkan([1, 2, 3, 4, 5]))
+    
