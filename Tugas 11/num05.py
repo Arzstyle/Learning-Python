bdd73c2c1378d946f1f690dead41f045f6853c8b
M Akbar Rizky Saputra <akbarrizky0909@gmail.com>
2024-05-15 10:47:39 +0700

    menghapus code

--- Tugas 11/num05.py
+++ Tugas 11/num05.py
@@ -9,17 +9,3 @@ def faktorial(n):
         return n * faktorial(n-1)
     
 print(faktorial(5))
-
-
-# Menggunakan For
-# def faktorial(n):
-#     hasil = 1
-#     for i in range(1, n + 1):
-#         hasil *= i
-#     return hasil
-
-# nilai = 5
-# hasil = faktorial(nilai)
-# print(hasil)
-
-
